1f5fd9f9ffd8037df1aa98f5166ce969d8493934
Luis de la Cruz <gael130819@gmail.com>
2022-08-26 01:04:16 -0500

    feat: update api restcountries

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,36 @@
 <template>
   <b-container fluid class="pl-0 pr-0">
-        <div class="content_title">
-          <h2 class="font-weight-bold">Where in the World?</h2>
-        </div>
-      
-        <div style="background:#FAFAFA">
-          <b-row class="content_header">
-            <b-col sm="4" cols="12" class="text-align-left">
-              <b-form-input v-model="countryName" placeholder="Search you country"></b-form-input>
-            </b-col>
-            <b-col sm="3" cols="12">
-              <b-form-select v-model="getCountriesByRegion" placeholder="Filter by Region" :options="options"></b-form-select>
-            </b-col>
-          </b-row>
-          <b-row class="content_body">
-            <b-col
-              v-for="country in countriesTotal"
-              :key="country.alpha2Code"
-              lg="4"
-              xl="3"
-            >
-              <b-card :img-src="country.flag" img-top class="mb-5">
-                <b-card-body>
-                  <h3> {{ country.name}}</h3>
-                  <h5>Population: {{ country.population}}</h5>
-                  <h5>Región: {{ country.region}}</h5>
-                  <h5>Caputal: {{ country.capital}}</h5>
-                </b-card-body>
-              </b-card>
-            </b-col>
-          </b-row>
-        </div>
+    <div class="content_title">
+      <h2 class="font-weight-bold">Where in the World?</h2>
+    </div>
+
+    <div style="background:#FAFAFA">
+      <b-row class="content_header">
+        <b-col sm="4" cols="12" class="text-align-left">
+          <b-form-input v-model="countryName" placeholder="Search you country"></b-form-input>
+        </b-col>
+        <b-col sm="3" cols="12">
+          <b-form-select v-model="getCountriesByRegion" placeholder="Filter by Region" :options="options"></b-form-select>
+        </b-col>
+      </b-row>
+      <b-row class="content_body">
+        <b-col
+          v-for="country in countriesTotal"
+          :key="country.alpha2Code"
+          lg="4"
+          xl="3"
+        >
+          <b-card :img-src="country.flag" img-top class="mb-5">
+            <b-card-body>
+              <h3> {{ country.name}}</h3>
+              <h5>Population: {{ country.population}}</h5>
+              <h5>Región: {{ country.region}}</h5>
+              <h5>Caputal: {{ country.capital}}</h5>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
   </b-container>
 </template>
 
@@ -52,20 +52,20 @@ export default {
           { value: 'Europe', text: 'Europe' },
           { value: 'Asia', text: 'Asia' },
           { value: 'Oceania', text: 'Oceania' },
-          
-        
+
+
         ]
     }
   },
   created() {
     axios
-      .get("https://restcountries.eu/rest/v2/all")
+      .get("https://restcountries.com/v2/all")
       .then((response) => {
         this.countriesTotal = response.data;
         this.countriesTotalOrigin = response.data;
         console.log(response.data)
       })
-      .catch((error) => 
+      .catch((error) =>
         console.log("No se encontró la información", error)
       );
   },
@@ -86,7 +86,7 @@ export default {
     },
     getCountriesByRegion(newValue) {
       if(newValue) {
-        this.countriesTotal = this.countriesTotal.filter(
+        this.countriesTotal = this.countriesTotalOrigin.filter(
           (country) => country.region == newValue
         )
       }
